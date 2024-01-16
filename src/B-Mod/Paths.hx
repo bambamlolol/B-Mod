a16c0dbb99be1e5d9a169f10ced65904a2153511
BamBamLol <82470343+bambamlolol@users.noreply.github.com>
2024-01-16 09:45:55 -0600

    Update Paths.hx

--- src/B-Mod/Paths.hx
+++ src/B-Mod/Paths.hx
@@ -1,16 +1,45 @@
 package B-Mod;
 
-import openfl.utils.Assets;
-
 class Paths {
-  public static var mod:String;
+	public static var mod:String;
+	var soundExtension = #if web "mp3" #else "ogg" #end;
 
-  public static function getImage(image:String) {
-    var path = "assets/" + mod + "/images/" + image + ".png";
-    if (sys.FileSystem.exists(path))
+	public static function getImage(image:String) {
+		var path = "assets/" + mod + "/images/" + image + ".png";
+		if (sys.FileSystem.exists(path))
+		{
 			return sys.io.File.getContent(path);
-    else
-      return null;
-      trace("ERROR: FILE: " + image + ".png COULD NOT BE FOUND";
-  }
+		}
+		else
+		{
+			return null;
+			trace("ERROR: FILE: " + image + ".png COULD NOT BE FOUND";
+		}
+	}
+			      
+	public static function getSound(sound:String) {
+		var path = "assets/" + mod + "/sounds/" + sound + "." + soundExtension;
+		if (sys.FileSystem.exists(path))
+		{
+			return sys.io.File.getContent(path);
+		}
+		else
+		{
+			return null;
+			trace("ERROR: FILE: " + sound + "." + soundExtension + " COULD NOT BE FOUND";
+		}
+	}
+			      
+	public static function getMusic(music:String) {
+		var path = "assets/" + mod + "/music/" + music + "." + soundExtension;
+		if (sys.FileSystem.exists(path))
+		{
+			return sys.io.File.getContent(path);
+		}
+		else
+		{
+			return null;
+			trace("ERROR: FILE: " + music + "." + soundExtension + " COULD NOT BE FOUND";
+		}
+	}
 }
