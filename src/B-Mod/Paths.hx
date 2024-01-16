00e6ee4d2d62b12187120baea7e614b55e759ab2
BamBamLol <82470343+bambamlolol@users.noreply.github.com>
2024-01-16 09:32:41 -0600

    Update Paths.hx

--- src/B-Mod/Paths.hx
+++ src/B-Mod/Paths.hx
@@ -1,5 +1,16 @@
 package B-Mod;
 
+import openfl.utils.Assets;
+
 class Paths {
   public static var mod:String;
+
+  public static function getImage(image:String) {
+    var path = "assets/" + mod + "/images/" + image + ".png";
+    if (sys.FileSystem.exists(path))
+			return sys.io.File.getContent(path);
+    else
+      return null;
+      trace("ERROR: FILE: " + image + ".png COULD NOT BE FOUND";
+  }
 }
